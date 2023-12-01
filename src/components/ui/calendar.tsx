f73a5290bb7fd059c2f61baf104b1e040eadc14f
momanyisamuel <momanyisamuel48@gmail.com>
2023-12-01 06:50:44 +0300

    first commit

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -58,8 +58,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        IconLeft: ({ ...props }) => <ChevronLeftIcon className="w-4 h-4" />,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        IconRight: ({ ...props }) => <ChevronRightIcon className="w-4 h-4" />,
       }}
       {...props}
     />
